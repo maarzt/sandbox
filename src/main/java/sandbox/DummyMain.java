c4dc17e91ca1e9186a5a8946f3f5f5f6f54b1807
Matthias Arzt <arzt@mpi-cbg.de>
2019-04-16 10:52:33 +0200

    Add simple pom-scijava based maven project

---
+++ src/main/java/sandbox/DummyMain.java
@@ -0,0 +1,8 @@
+package sandbox;
+
+public class DummyMain
+{
+	public static void main(String... args) {
+		System.out.println("Hello World!");
+	}
+}
---
+++ src/test/java/sandbox/DummyTest.java
@@ -0,0 +1,13 @@
+package sandbox;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class DummyTest
+{
+	@Test
+	public void test() {
+		assertTrue(true);
+	}
+}
