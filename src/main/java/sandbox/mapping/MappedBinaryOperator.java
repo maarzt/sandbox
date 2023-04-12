4422cadca8d8ccb5b5bde080c539c835929a7fd7
Matthias Arzt <arzt@mpi-cbg.de>
2023-04-12 14:26:11 +0200

    MappedBinaryOperator: fix thread safety issie

--- src/main/java/sandbox/mapping/MappedBinaryOperator.java
+++ src/main/java/sandbox/mapping/MappedBinaryOperator.java
@@ -1,19 +1,20 @@
 package sandbox.mapping;
 
-import gnu.trove.map.TIntIntMap;
 import gnu.trove.map.TLongIntMap;
-import gnu.trove.map.hash.TIntIntHashMap;
 import gnu.trove.map.hash.TLongIntHashMap;
 
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 import java.util.function.BinaryOperator;
 import java.util.function.IntBinaryOperator;
-import java.util.function.IntUnaryOperator;
 
 public class MappedBinaryOperator< T > implements IntBinaryOperator {
 
 	private final ValueMapping< T > mapping;
 
 	private final BinaryOperator< T > operator;
+
+	private final ReadWriteLock lock = new ReentrantReadWriteLock();
 
 	private final TLongIntMap indexOperator = new TLongIntHashMap();
 
@@ -27,16 +28,30 @@ public class MappedBinaryOperator< T > implements IntBinaryOperator {
 	@Override
 	public int applyAsInt(int a, int b) {
 		long index = (long) a << 32L | b;
-		int resultIndex = indexOperator.get(index);
-		if( resultIndex != indexOperator.getNoEntryKey() )
-			return resultIndex;
-		synchronized ( indexOperator ) {
-			resultIndex = indexOperator.get(index);
+		lock.readLock().lock();
+		try
+		{
+			int resultIndex = indexOperator.get( index );
+			if ( resultIndex != indexOperator.getNoEntryKey() )
+				return resultIndex;
+		}
+		finally
+		{
+			lock.readLock().unlock();
+		}
+		lock.writeLock().lock();
+		try
+		{
+			int resultIndex = indexOperator.get(index);
 			if( resultIndex != indexOperator.getNoEntryKey() )
 				return resultIndex;
 			resultIndex = mapping.getIndex(operator.apply(mapping.getValue(a), mapping.getValue(b)));
 			indexOperator.put(index, resultIndex);
 			return resultIndex;
+		}
+		finally
+		{
+			lock.writeLock().unlock();
 		}
 	}
 }
